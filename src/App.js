42aee45ef0e00b93c864b36fc2ed426c53c66b6c
wjcooper91 <wjcooper91@gmail.com>
2018-09-27 06:39:57 -0400

    updated images

--- src/App.js
+++ src/App.js
@@ -45,10 +45,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">The Clickster</h1>
-          <p className="App-intro">
-            Try not to click the same image twice!
-          </p>
+          <h1 className="App-title">Clicky App</h1>
         </header>
         <Score total={this.state.score}
                goal={8}
@@ -64,10 +61,6 @@ class App extends Component {
             />
           ))}
         </Wrapper>
-        <footer>
-          <p>Designed and built by Heather Mathies. You can find the
-          code<a href="https://github.com/hmathies/Clicky-Game" target="_blank" rel="noopener noreferrer"> here</a>.</p>
-        </footer>
     </div>
     );
   }
